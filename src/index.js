4fb50d25f32823c76ecb06a982dc331aeae65cc7
sunny <sunnya2023@gmail.com>
2023-11-16 19:27:41 +0900

    04 scss셋팅하기

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import './assets/scss/style.scss';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
