822a9f747b3f8304423519094c64b1b63230c962
sunny <sunnya2023@gmail.com>
2023-11-26 21:03:47 +0900

    페이지 만들기

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import ReactDOM from 'react-dom'; //구버전
+import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import './assets/scss/style.scss';
