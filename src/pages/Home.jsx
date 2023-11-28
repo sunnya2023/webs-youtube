0dedb512497659d39942c835ce701572fd0c8051
sunny <sunnya2023@gmail.com>
2023-11-28 21:59:33 +0900

    메인 컨텐츠 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import Main from "../components/section/Main";
+import Today from "../components/contents/Developer";
+import Developer from "../components/contents/Webd";
+import Webd from "../components/contents/Developer";
+import Website from "../components/contents/Website";
+import Gsap from "../components/contents/Gsap";
+import Port from "../components/contents/Port";
+import Youtube from "../components/contents/Youtube";
 
 const Home = () => {
 	return (
@@ -7,7 +14,13 @@ const Home = () => {
 			title="웹스토리보이 유튜브 채널"
 			description="웹스트리보이 유튜브 채널에 오신것을 환영합니다.."
 		>
-			Home
+			<Today />
+			<Developer />
+			<Webd />
+			<Website />
+			<Gsap />
+			<Port />
+			<Youtube />
 		</Main>
 	);
 };
