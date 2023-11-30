fab417abe146b758e5d2ba5b2781ba2fc5646326
sunny <sunnya2023@gmail.com>
2023-12-01 02:08:37 +0900

    비디오  컴포넌트 통합 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Main from "../components/section/Main";
 import Today from "../components/contents/Today";
 import Developer from "../components/contents/Developer";
-import Webd from "../components/contents/Webd";
-import Website from "../components/contents/Website";
-import Gsap from "../components/contents/Gsap";
-import Port from "../components/contents/Port";
-import Youtube from "../components/contents/Youtube";
+import VideoSlider from "../components/video/VideoSlider";
+import { webdText } from "../data/webd";
+import { websiteText } from "../data/website";
+import { gsapText } from "../data/gsap";
+import { portfolioText } from "../data/portfolio";
+import { youtubeText } from "../data/youtube";
 
 const Home = () => {
 	return (
@@ -16,11 +17,36 @@ const Home = () => {
 		>
 			<Today />
 			<Developer />
-			<Webd />
-			<Website />
-			<Gsap />
-			<Port />
-			<Youtube />
+			<VideoSlider
+				videos={webdText}
+				title="😮 웹디자인기능사 준비는 이걸로!"
+				id="web"
+			/>
+			<VideoSlider
+				videos={websiteText}
+				title="😛 웹표준 사이트 만들기 기초 다지기!"
+				id="website"
+			/>
+			<VideoSlider
+				videos={gsapText}
+				title="🤓 GSAP 패럴랙스 효과를 하고 싶다면!"
+				id="gsap"
+			/>
+			<VideoSlider
+				videos={gsapText}
+				title="🤓 GSAP 패럴랙스 효과를 하고 싶다면!"
+				id="gsap"
+			/>
+			<VideoSlider
+				videos={portfolioText}
+				title="🤗 포트폴리오 만드는 방법을 공유합니다."
+				id="portfolio"
+			/>
+			<VideoSlider
+				videos={youtubeText}
+				title="😱 지금 이 코딩을 영상으로"
+				id="youtube"
+			/>
 		</Main>
 	);
 };
